f84007fe3670ff5d4b3cbb6991d39da787eede73
carlos-henrique-dos-santos <carlos.hrq.rodrigues@gmail.com>
2021-10-29 19:04:21 -0300

    adiciona lista de tarefas

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,14 @@
       <button>Add new todo</button>
   </form>
 
+  <ul>
+    <li v-for="todo in todos" :key="todo.id" class="todo">
+      <h3 :class="{ estaCompleta: todo.estaCompleta }" @click="toggleDone(todo)">
+        {{todo.descricao}}
+      </h3>
+    </li>
+  </ul>
+
 </template>
 
 <script>
@@ -14,14 +22,26 @@ export default {
   setup() {
 
     const newTodo = ref('');
+    const todos = ref([]);
 
-    function addNewTodo() {
-      console.log(newTodo.value)
+    function addNewTodo() {      
+      todos.value.push({
+        id: todos.value.length,
+        estaCompleta: false,
+        descricao: newTodo.value
+      });
+      newTodo.value = "";
+    }
+
+    function toggleDone(todo) {
+      todo.estaCompleta = !todo.estaCompleta;
     }
 
     return {
       newTodo,
       addNewTodo,
+      todos,
+      toggleDone
     }
   }
 }
@@ -36,4 +56,13 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
+.todo {
+  cursor: pointer;
+}
+
+.estaCompleta {
+  text-decoration: line-through;
+}
+
 </style>
