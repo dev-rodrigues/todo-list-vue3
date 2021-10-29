0a3c884dcf51e9ac7241e5faa1c1e1534c039ddd
carlos-henrique-dos-santos <carlos.hrq.rodrigues@gmail.com>
2021-10-29 19:43:54 -0300

    adiciona estilizacao

--- src/App.vue
+++ src/App.vue
@@ -1,68 +1,110 @@
 <template>
+
+  <div class="wrapper">
     <form @submit.prevent="addNewTodo">
       <label>Todo:</label>
       <input v-model="newTodo" name="newTodo">
       <button>Add new todo</button>
-  </form>
+    </form>
 
-  <ul>
-    <li v-for="todo in todos" :key="todo.id" class="todo">
-      <h3 :class="{ estaCompleta: todo.estaCompleta }" @click="toggleDone(todo)">
-        {{todo.descricao}}
-      </h3>
-    </li>
-  </ul>
-
+    <ul>
+      <li v-for="todo in todos" :key="todo.id" class="todo">
+        <h3 :class="{ estaCompleta: todo.estaCompleta }" @click="toggleDone(todo)">
+          {{todo.descricao}}
+        </h3>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
-import { ref } from 'vue';
+  import { ref } from 'vue';
 
-export default {
-  setup() {
+  export default {
+    setup() {
 
-    const newTodo = ref('');
-    const todos = ref([]);
+      const newTodo = ref('');
+      const todos = ref([]);
 
-    function addNewTodo() {      
-      todos.value.push({
-        id: todos.value.length,
-        estaCompleta: false,
-        descricao: newTodo.value
-      });
-      newTodo.value = "";
-    }
+      function addNewTodo() {
+        if (newTodo.value === '') {
+          alert('preencha o nome da tarefa')
+          
+        } else {
+          todos.value.push({
+            id: todos.value.length,
+            estaCompleta: false,
+            descricao: newTodo.value
+          });
+          newTodo.value = "";
+        }
+        
+      }
 
-    function toggleDone(todo) {
-      todo.estaCompleta = !todo.estaCompleta;
-    }
+      function toggleDone(todo) {
+        todo.estaCompleta = !todo.estaCompleta;
+      }
 
-    return {
-      newTodo,
-      addNewTodo,
-      todos,
-      toggleDone
+      return {
+        newTodo,
+        addNewTodo,
+        todos,
+        toggleDone
+      }
     }
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 
-.todo {
-  cursor: pointer;
-}
+  #app {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    
+    widows: 100%;
+    display: flex;
+    flex-direction: column;
+  }
 
-.estaCompleta {
-  text-decoration: line-through;
-}
+  #wrapper {
+    width: 920px;
+  }
+
+  form {  
+    display: flex;
+    flex-direction: column;
+
+    align-items: center;
+    align-content: center;
+
+  }
+
+  input {
+    height: 10px;
+    width: 920px;
+    font-size: 15px;
+    padding: 5px;
+  }  
+  
+  button {
+    border: 0;
+    width: 920px;
+    padding: 0.6rem;
+    border-radius: 0.3rem;
+    font-size: 0.9rem;
+    margin-top: 0.5rem;
+    background-color: #17273f;
+    color: white;
+  }
+
+  .todo {
+    cursor: pointer;
+  }
+
+  .estaCompleta {
+    text-decoration: line-through;
+    color: red;
+  }
 
 </style>
